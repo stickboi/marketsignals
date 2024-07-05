ff7242c2da7e1f4157835c57747651cc182676bd
Lucien Black <lab23242@gmail.com>
2024-07-05 12:45:00 +0100

    Bollinger bands + various std functions

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = yf.download("GOOG AAPL", period="1y")
-
+data.to_csv("data.csv")
 #print(data.head())
 td = (data["Close"]["AAPL"]- data["Close"]["GOOG"]).to_frame()
 
 td["tdma"] = td[0].rolling(20).mean()
-td["bu"] = td["tdma"]+2*td["tdma"].rolling(20).std()
-td["bl"] = td["tdma"]-2*td["tdma"].rolling(20).std()
-td.dropna(inplace = True)
+td["std"] = td[0].rolling(20).std(ddof=0)
+td["bu"] = td["tdma"]+2*td["std"]
+td["bl"] = td["tdma"]-2*td["std"]
+#td["np-std"] = td[0].rolling(20).apply(np.std)
+#td["pd-std"] = td[0].rolling(20).std(ddof=0)
 
 plot = td.plot()
 plt.show()
 print(td)
+td.to_csv("td.csv")
